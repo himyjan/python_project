4e480856c977c2a8ed24896bcf673608030d17a1
himyjan <51815522+himyjan@users.noreply.github.com>
2023-10-27 18:04:45 +0800

    v2 upload

--- src/python_project/__init__.py
+++ src/python_project/__init__.py
@@ -6,6 +6,18 @@ import flet as ft
 from flet.matplotlib_chart import MatplotlibChart
 import os
 
+from flet import AppBar
+from flet import Card
+from flet import Column
+from flet import Container
+from flet import ElevatedButton
+from flet import IconButton
+from flet import Row
+from flet import Switch
+from flet import Text
+from flet import colors
+from flet import icons
+
 matplotlib.use("svg")
 
 # https://colab.research.google.com/github/willismax/matplotlib_show_chinese_in_colab/blob/master/matplotlib_show_chinese_in_colab.ipynb#scrollTo=RfR0uymWF3cB
@@ -26,15 +38,14 @@ from responsive_menu_layout import ResponsiveMenuLayout
 
 import numpy as np
 import pandas as pd
-import json
-def show_bar(ax, csv_data):
+def show_bar(ax, csv_data, json_data):
     # # 分析CSV數據（範例：新北市各區站點數量和剩餘車量）
     # site_count_by_area = csv_data.groupby('鄉鎮市區')['站點名稱'].count()
     # remaining_bikes_by_area = csv_data.groupby('區域')['剩餘車量'].sum()
 
     # 生成CSV數據的圖表
     # 将csv_data.head(5).to_numpy()转换为list类型的数据
-    data_list = csv_data.head(10).to_numpy()
+    data_list = csv_data.head(5).to_numpy()
     data_list_sorted = data_list[data_list[:, 2].argsort()]
     ax[0].plot(data_list_sorted[:,1].tolist(),data_list_sorted[:,2].tolist())
     ax[0].set_ylabel('金額(千元)')
@@ -61,10 +72,16 @@ def show_bar(ax, csv_data):
     # towns = list(town_pop_density.keys())
     # densities = list(town_pop_density.values())
     # ax.barh(towns, densities)
-    # ax.xlabel('人口密度')
-    # ax.title('各鄉鎮市區人口密度')
     # ax.tight_layout()
     # ax.show()
+    
+    data_list = json_data.head(10).to_numpy()
+    data_list_sorted = data_list[data_list[:, 2].argsort()]
+    print(data_list_sorted)
+    ax[1].plot(data_list_sorted[:,1].tolist(),data_list_sorted[:,2].tolist())
+    ax[1].set_xlabel('人口密度')
+    ax[1].set_title('各鄉鎮市區人口密度')
+    ax[1].grid(True)
 
     return ax
 
@@ -82,49 +99,194 @@ DEFAULT_FLET_PORT = 8502
 def main(page: ft.Page):
     page.title = "You Enjoy Mychatbot"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    menu_button = IconButton(icons.MENU)
+
+    page.appbar = AppBar(
+        leading=menu_button,
+        leading_width=40,
+        bgcolor=colors.SURFACE_VARIANT,
+    )
 
     # 讀取CSV數據
-    csv_data = pd.read_csv('https://eip.fia.gov.tw/data/ias/ias109/109_165-9.csv')
+    csv_data = pd.read_csv('./src/python_project/109_165-9.csv')
+    json_data = pd.read_json('./src/python_project/10909.json')
     fig, ax = plt.subplots(2, 1)
-    ax = show_bar(ax, csv_data)
+    ax = show_bar(ax, csv_data, json_data)
     simpledt_dt = show_data_list(csv_data.head(5))
 
     def route_change(route):
-        page.views.clear()
-        page.views.append(
-            ft.View(
-                "/",
+        # page.views.clear()
+        pages = [
+            (
+                dict(icon=icons.LANDSCAPE_OUTLINED, selected_icon=icons.LANDSCAPE, label="Menu in landscape"),
+                create_page(
+                    "Menu in landscape",
+                    "Menu in landscape is by default shown, side by side with the main content, but can be "
+                    "hidden with the menu button.",
+                ),
+            ),
+            (
+                dict(icon=icons.PORTRAIT_OUTLINED, selected_icon=icons.PORTRAIT, label="Menu in portrait"),
+                create_page(
+                    "Menu in portrait",
+                    "Menu in portrait is mainly expected to be used on a smaller mobile device."
+                    "\n\n"
+                    "The menu is by default hidden, and when shown with the menu button it is placed on top of the main "
+                    "content."
+                    "\n\n"
+                    "In addition to the menu button, menu can be dismissed by a tap/click on the main content area.",
+                ),
+            ),
+            (
+                dict(
+                    icon=icons.INSERT_EMOTICON_OUTLINED, selected_icon=icons.INSERT_EMOTICON, label="Minimize to icons"
+                ),
+                create_page(
+                    "Minimize to icons",
+                    "ResponsiveMenuLayout has a parameter minimize_to_icons. "
+                    "Set it to True and the menu is shown as icons only, when normally it would be hidden.\n"
+                    "\n\n"
+                    "Try this with the 'Minimize to icons' toggle in the top bar."
+                    "\n\n"
+                    "There are also landscape_minimize_to_icons and portrait_minimize_to_icons properties that you can "
+                    "use to set this property differently in each orientation.",
+                ),
+            ),
+            (
+                dict(icon=icons.COMPARE_ARROWS_OUTLINED, selected_icon=icons.COMPARE_ARROWS, label="Menu width"),
+                create_page(
+                    "Menu width",
+                    "ResponsiveMenuLayout has a parameter manu_extended. "
+                    "Set it to False to place menu labels under the icons instead of beside them."
+                    "\n\n"
+                    "Try this with the 'Menu width' toggle in the top bar.",
+                ),
+            ),
+            (
+                dict(icon=icons.ROUTE_OUTLINED, selected_icon=icons.ROUTE, label="Route support", route="custom-route"),
+                create_page(
+                    "Route support",
+                    "ResponsiveMenuLayout has a parameter support_routes, which is True by default. "
+                    "\n\n"
+                    "Routes are useful only in the web, where the currently selected page is shown in the url, "
+                    "and you can open the app directly on a specific page with the right url."
+                    "\n\n"
+                    "You can specify a route explicitly with a 'route' item in the menu dict (see this page in code). "
+                    "If you do not specify the route, a slugified version of the page label is used "
+                    "('Menu width' becomes 'menu-width').",
+                ),
+            ),
+            (
+                dict(icon=icons.PLUS_ONE_OUTLINED, selected_icon=icons.PLUS_ONE, label="Fine control"),
+                create_page(
+                    "Adjust navigation rail",
+                    "NavigationRail is accessible via the navigation_rail attribute of the ResponsiveMenuLayout. "
+                    "In this demo it is used to add the leading button control."
+                    "\n\n"
+                    "These NavigationRail attributes are used by the ResponsiveMenuLayout, and changing them directly "
+                    "will probably break it:\n"
+                    "- destinations\n"
+                    "- extended\n"
+                    "- label_type\n"
+                    "- on_change\n",
+                ),
+            ),
+        ]
+
+        menu_layout = ResponsiveMenuLayout(page, pages)
+
+        page.appbar.actions = [
+            Row(
                 [
-                    ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT),
-                    MatplotlibChart(fig, expand=True),
-                    simpledt_dt,
-                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
-                ],
+                    Text("Minimize\nto icons"),
+                    Switch(on_change=lambda e: toggle_icons_only(menu_layout)),
+                    Text("Menu\nwidth"),
+                    Switch(value=True, on_change=lambda e: toggle_menu_width(menu_layout)),
+                ]
             )
+        ]
+
+        menu_layout.navigation_rail.leading = ElevatedButton(
+            "Add", icon=icons.ADD, expand=True, on_click=lambda e: print("Add clicked")
         )
-        if page.route == "/store":
-            page.views.append(
-                ft.View(
-                    "/store",
-                    [
-                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
-                    ],
-                )
-            )
-        page.update()
+
+        page.add(menu_layout)
+
+        menu_button.on_click = lambda e: menu_layout.toggle_navigation()
+
+        # page.views.append(
+        #     ft.View(
+        #         "/",
+        #         [
+        #             ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT),
+        #             menu_layout,
+        #             # MatplotlibChart(fig, expand=True),
+        #             # simpledt_dt,
+        #             ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
+        #         ],
+        #     )
+        # )
+        # if page.route == "/store":
+        #     page.views.append(
+        #         ft.View(
+        #             "/store",
+        #             [
+        #                 ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+        #                 ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+        #             ],
+        #         )
+        #     )
+        # page.update()
 
     def view_pop(view):
         page.views.pop()
         top_view = page.views[-1]
         page.go(top_view.route)
+    
+    def create_page(title: str, body: str):
+        return Row(
+            controls=[
+                Column(
+                    horizontal_alignment="stretch",
+                    controls=[
+                        Card(content=Container(Text(title, weight="bold"), padding=8)),
+                        Text(body),
+                    ],
+                    expand=True,
+                ),
+            ],
+            expand=True,
+        )
+    
+    def toggle_icons_only(menu: ResponsiveMenuLayout):
+        menu.minimize_to_icons = not menu.minimize_to_icons
+        menu.page.update()
+
+    def toggle_menu_width(menu: ResponsiveMenuLayout):
+        menu.menu_extended = not menu.menu_extended
+        menu.page.update()
 
     page.on_route_change = route_change
     page.on_view_pop = view_pop
-    page.go(page.route)
+    # page.go(page.route)
     page.add(ft.Text("Reba put a stopper in the bottom of the tub"))
 
 if __name__ == "__main__":
     flet_path = os.getenv("FLET_PATH", DEFAULT_FLET_PATH)
     flet_port = int(os.getenv("FLET_PORT", DEFAULT_FLET_PORT))
     ft.app(name=flet_path, target=main, view=None, port=flet_port)
+
+# import pandas as pd
+# import os
+
+# df = pd.read_excel('./src/python_project/廚具logo設計 (回覆)1.xlsx')
+# column_name = '一個傢具行的商品，您希望怎麼樣的呈現最佳，請選擇兩項。'
+# replace_symbols = ['>', '<', ':', '"', '/', '\\\\', '\|', '\?', '\*']
+# df[column_name] = (
+#     df[column_name].replace(replace_symbols, '', regex=True).str.strip().str.title()
+# )
+# unique_values = df[column_name].unique()
+
+# for unique_value in unique_values:
+#     df_output = df[df[column_name].str.contains(unique_value)]
+#     df_output.to_excel('./廚具logo設計 (回覆)2.xlsx', sheet_name='廚具logo設計 (回覆)2.xlsx', index=False)
